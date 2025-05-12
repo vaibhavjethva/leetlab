78917d10f8f1a2953564a8dd57ecfa6c054a081e
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2025-05-12 22:21:32 +0530

    feat: implement create problem functionality

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,95 @@
+import { db } from "../libs/db.js";
+
+export const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+
+  if (req.user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ error: "You are not allowed to create a problem" });
+  }
+
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        return res.status(400).json({
+          error: `Language ${language} is not supported`,
+        });
+      }
+
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log("Result-----", result);
+        // console.log(
+        //   `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+        // );
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+    }
+
+    const newProblem = await db.problem.create({
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user.id,
+      },
+    });
+
+    return res.status(201).json({
+      sucess: true,
+      message: "Message Created Successfully",
+      problem: newProblem,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While Creating Problem",
+    });
+  }
+};
+
+export const getAllProblems = async (req, res) => {};
+
+export const getProblemById = async (req, res) => {};
+
+export const updateProblem = async (req, res) => {};
+
+export const deleteProblem = async (req, res) => {};
+
+export const getAllProblemsSolvedByUser = async (req, res) => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/problems", problemRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port 8080");
