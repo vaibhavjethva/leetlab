7ef81ace3de4fc5c9b31b5f3bce91346765e62c5
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2025-05-28 22:15:22 +0530

    feat: add executionCode logic for handling code execution flow

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,4 +1,9 @@
-import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
+import {
+  getLanguageName,
+  pollBatchResults,
+  submitBatch,
+} from "../libs/judge0.lib.js";
+import { db } from "../libs/db.js";
 
 export const executeCode = async (req, res) => {
   try {
@@ -36,9 +41,112 @@ export const executeCode = async (req, res) => {
     console.log("Result-------------");
     console.log(results);
 
+    // Analyze testcase results
+    let allPassed = true;
+    const detailedResults = results.map((result, i) => {
+      const stdout = result.stdout?.trim();
+      const expected_output = expected_outputs[i]?.trim();
+      const passed = stdout === expected_output;
+
+      if (!passed) allPassed = false;
+
+      return {
+        testCase: i + 1,
+        passed,
+        stdout,
+        expected: expected_output,
+        stderr: result.stderr || null,
+        compile_output: result.compile_output || null,
+        status: result.status.description,
+        memory: result.memory ? `${result.memory} KB` : undefined,
+        time: result.time ? `${result.time} s` : undefined,
+      };
+
+      // console.log(`Testcase #${i + 1}`);
+      // console.log(`Input for testcase #${i + 1}: ${stdin[i]}`);
+      // console.log(`Expected Output for testcase #${i + 1}: ${expected_output}`);
+      // console.log(`Actual output for testcase #${i + 1}: ${stdout}`);
+      // console.log(`Matched testcase #${i + 1}: ${passed}`);
+    });
+
+    console.log(detailedResults);
+
+    // store submission summary
+    const submission = await db.submission.create({
+      data: {
+        userId,
+        problemId,
+        sourceCode: source_code,
+        language: getLanguageName(language_id),
+        stdin: stdin.join("\n"),
+        stdout: JSON.stringify(detailedResults.map((r) => r.stdout)),
+        stderr: detailedResults.some((r) => r.stderr)
+          ? JSON.stringify(detailedResults.map((r) => r.stderr))
+          : null,
+        compileOutput: detailedResults.some((r) => r.compile_output)
+          ? JSON.stringify(detailedResults.map((r) => r.compile_output))
+          : null,
+        status: allPassed ? "Accepted" : "Wrong Answer",
+        memory: detailedResults.some((r) => r.memory)
+          ? JSON.stringify(detailedResults.map((r) => r.memory))
+          : null,
+        time: detailedResults.some((r) => r.time)
+          ? JSON.stringify(detailedResults.map((r) => r.time))
+          : null,
+      },
+    });
+
+    // If All passed = true mark problem as solved for the current user
+    if (allPassed) {
+      await db.problemSolved.upsert({
+        where: {
+          userId_problemId: {
+            userId,
+            problemId,
+          },
+        },
+        update: {},
+        create: {
+          userId,
+          problemId,
+        },
+      });
+    }
+
+    // Save individual test case results  using detailedResult
+    const testCaseResults = detailedResults.map((result) => ({
+      submissionId: submission.id,
+      testCase: result.testCase,
+      passed: result.passed,
+      stdout: result.stdout,
+      expected: result.expected,
+      stderr: result.stderr,
+      compileOutput: result.compile_output,
+      status: result.status,
+      memory: result.memory,
+      time: result.time,
+    }));
+
+    await db.testCaseResult.createMany({
+      data: testCaseResults,
+    });
+
+    const submissionWithTestCase = await db.submission.findUnique({
+      where: {
+        id: submission.id,
+      },
+      include: {
+        testCases: true,
+      },
+    });
+
     res.status(200).json({
       success: true,
       message: "Code Executed! Successfully!",
+      submission: submissionWithTestCase,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error executing code:", error.message);
+    res.status(500).json({ error: "Failed to execute code" });
+  }
 };
