62d1fd4738ca483e2bc6b718a158fa82419d69f6
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2025-05-28 22:46:47 +0530

    feat: add submissions feature for fetching by problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.route.js";
 import executionRoute from "./routes/executeCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port 8080");
