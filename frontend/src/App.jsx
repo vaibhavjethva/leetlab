387e1bb0d302cd75e7ec5470f9ff952c8ec79067
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2025-06-02 23:44:26 +0530

    feat: setup frontend and implement signup page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
+
+const App = () => {
+  let authUser = null;
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
